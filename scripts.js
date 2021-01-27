d4e5c9f98e153bac484d4cdbf43ea558ba669976
Erjankydyrov <Erjankydyrov29@gmail.com>
2021-01-27 13:49:14 +0600

    added do-1

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 // table
 
 for (let i1 = 1; i1 <= 9; i1++) {  
-    document.write("<div style='float: left; width: 70px; margin-right: 10px'>"); 
+    document.write("<div style='float: left; width: 70px; margin-right: 10px; border: 2px solid'>"); 
     for (let i2 = 1; i2 <= 9; i2++) { 
         document.write(i2 + "*" + i1 + "=" +(i2*i1) + "<br>"); 
     }  
     document.write ("</div>"); 
 } 
 
+let y = 5;
+do {
+    document.write(y + "<br>");
+    y++;
+} while (y <= 10);
