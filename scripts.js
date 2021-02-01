6ab55deffdac13377be2cb69b0a84c10f6363fbe
Erjankydyrov <Erjankydyrov29@gmail.com>
2021-02-01 22:12:16 +0600

    ura! I did it!

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 // table
 
+// for (let i1 = 1; i1 <= 9; i1++) {  
+//     document.write("<div style='float: left; width: 70px; margin-right: 10px; border: 2px solid'>"); 
+//     for (let i2 = 1; i2 <= 9; i2++) { 
+//         document.write(i2 + "*" + i1 + "=" +(i2*i1) + "<br>"); 
+//     }  
+//     document.write ("</div>"); 
+// } 
+
+document.write('<table>');
 for (let i1 = 1; i1 <= 9; i1++) {  
-    document.write("<div style='float: left; width: 70px; margin-right: 10px; border: 2px solid'>"); 
+    document.write("<tr>"); 
     for (let i2 = 1; i2 <= 9; i2++) { 
-        document.write(i2 + "*" + i1 + "=" +(i2*i1) + "<br>"); 
+        document.write("<td>" + i2 + "*" + i1 + "=" +(i2*i1) + "<td>" + "<br>"); 
     }  
-    document.write ("</div>"); 
+    document.write ("</tr>"); 
 } 
+document.write('</table>');
 
 // let y = 5;
 // do {
@@ -26,8 +36,8 @@ for (let i1 = 1; i1 <= 9; i1++) {
 //     t--;
 // } while (t >= 1);
 
-let r = 3;
-do {
-    document.write(r + "<br>");
-    r++;
-} while (r <= 50);
+// let r = 3;
+// do {
+//     document.write(r + "<br>");
+//     r++;
+// } while (r <= 50);
